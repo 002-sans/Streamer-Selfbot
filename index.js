9cf7e610fb6460f4903fff48e62ae0395987711b
Sans <sans002@outlook.fr>
2024-04-19 14:16:59 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const StreamVideo  = require('@dank074/discord-video-stream'),     
-    ffmpegPath     = require('@ffmpeg-installer/ffmpeg').path,
-    Discord        = require('discord.js-selfbot-v13'),
-    config         = require('./config.json'),
-    ffmpeg         = require('fluent-ffmpeg')
-                     require('colors')
+      ffmpegPath   = require('@ffmpeg-installer/ffmpeg').path,
+      Discord      = require('discord.js-selfbot-v13'),
+      config       = require('./config.json')
+      ffmpeg       = require('fluent-ffmpeg'),
+      db           = require('./db.json'),
+      fs           = require('node:fs')
+                    require('colors')
+                    ffmpeg.setFfmpegPath(ffmpegPath)
 
-ffmpeg.setFfmpegPath(ffmpegPath)
 
+console.clear()
 console.log(`
 .=====================================================================================================================.
 |                          ███████╗████████╗██████╗ ███████╗ █████╗ ███╗   ███╗███████╗██████╗                        |
@@ -19,7 +22,6 @@ console.log(`
 console.log("{ Logs: }\n".red)
 const streamer = new StreamVideo.Streamer(new Discord.Client());
 streamer.client.login(config.token).catch(e => console.log(`[🔴] | ${e}`.red));
-
 streamer.client.on('ready', async client => console.log(`[🟢] | ${client.user.username} est connecté`.green));
 streamer.client.on('messageCreate', async message => {
     if (message.author.id !== streamer.client.user.id &&
@@ -29,7 +31,7 @@ streamer.client.on('messageCreate', async message => {
 
     switch(args[0]){
         case "help":
-            message.channel.send(`
+            editMessage(streamer.client, message, `
             \`${config.prefix}help\` ➜ **Affiche ce menu**
             \`${config.prefix}ping\` ➜ **Montre le ping du bot**
             \`${config.prefix}play <lien>\` ➜ **Stream une vidéo (.mp4)**
@@ -39,15 +41,15 @@ streamer.client.on('messageCreate', async message => {
             break
 
         case "ping":
-            const msg = await message.channel.send(`Ping: \`${streamer.client.ws.ping}\`ms`).then(m => deletemsg(m, 10))
-            //msg.edit(`Ping: \`${streamer.client.ws.ping}\`ms\nLatance: \`${msg.createdAt - message.createdAt}\`ms`).then(m => deletemsg(m, 10))
+            const msg = await message.channel.send(`Ping: \`${streamer.client.ws.ping}\`ms`)
+            msg.edit(`Ping: \`${streamer.client.ws.ping}\`ms\nLatance: \`${msg.createdAt - message.createdAt}\`ms`).then(m => deletemsg(m, 10))
             break
 
         case "play":
-            if (!message.guild) return message.channel.send("Veuillez utiliser cette commande sur un serveur !").then(m => deletemsg(m, 10))
-            if (!message.member.voice) return message.channel.send("Veuillez rejoindre un salon vocale !").then(m => deletemsg(m, 10))
-            if (!message.member.voice.channel.joinable) return message.channel.send("Je ne peux pas rejoindre le salon vocale !").then(m => deletemsg(m, 10))
-            if (!args[1] || !args[1].startsWith("https://")) return message.channel.send("Veuillez me donner un lien de vidéo valide !").then(m => deletemsg(m, 10))
+            if (!message.guild) return editMessage(streamer.client, message, "Veuillez utiliser cette commande sur un serveur !")
+            if (!message.member.voice.channel) return editMessage(streamer.client, message, "Veuillez rejoindre un salon vocale !")
+            if (!message.member.voice.channel.joinable) return editMessage(streamer.client, message, "Je ne peux pas rejoindre le salon vocale !")
+            if (!args[1] || !args[1].startsWith("http")) return editMessage(streamer.client, message, "Veuillez me donner un lien de vidéo valide !")
 
             await streamer.joinVoice(message.guild.id, message.member.voice.channel.id).catch(() => false)
             const udp = await streamer.createStream();
@@ -55,7 +57,7 @@ streamer.client.on('messageCreate', async message => {
             udp.mediaConnection.setVideoStatus(true);
             try {
                 console.log(`[✉️] | Commande ${args[0]} executée`.yellow)
-                message.channel.send(`Lancement du stream sur ${message.member.voice.channel} !`).then(m => deletemsg(m, 10))
+                editMessage(streamer.client, message, `Lancement du stream sur ${message.member.voice.channel} !`)
                 await StreamVideo.streamLivestreamVideo(args[1], udp);
             } catch (e) { 
                 message.channel.send(`${e}`).then(m => deletemsg(m, 10))
@@ -68,21 +70,27 @@ streamer.client.on('messageCreate', async message => {
 
         case "stop":
             streamer.stopStream()
-            message.channel.send("Le stream a été arrêté !").then(m => deletemsg(m, 10))
+            editMessage(streamer.client, message, "Le stream a été arrêté !")
             console.log(`[✉️] | Commande ${args[0]} executée`.yellow)
             break
 
         case "leave":
             streamer.leaveVoice()
-            message.channel.send("J'ai quitté le salon vocale !").then(m => deletemsg(m, 10))
+            editMessage(streamer.client, message, "J'ai quitté le salon vocale !")
             console.log(`[✉️] | Commande ${args[0]} executée`.yellow)
             break
+
     }
 })
 
 
 function deletemsg(msg, ms){
     setTimeout(() => {
-        if (msg?.deletable) message.delete().catch(() => false)
+        if (msg.deletable) message.delete().catch(() => false)
     }, ms * 1000 * 60);
+}
+
+function editMessage(client, message, text){
+    if (message.author.id === client.user.id) message.edit(text).then(m => deletemsg(m, 10)).catch(() => message.delete().catch(() => false))
+    else message.channel.send(text).then(m => deletemsg(m, 10)).catch(() => message.delete().catch(() => false))
 }
